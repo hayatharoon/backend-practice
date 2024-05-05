0278ef0bfa13984325585dec8ca70c6c07a50c77
Haroon Hayat <hayat_haroon@yahoo.com>
2024-05-05 16:54:34 +0500

    expense modal complete

---
+++ models/expense.js
@@ -0,0 +1,55 @@
+const moment = require('moment');
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+const Expense = sequelize.define(
+  'expense',
+  {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    paid: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    payType: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    receiptNo: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: (record) => {
+        (record.createdAt = moment().unix()), (record.updatedAt = moment().unix());
+      },
+      beforeUpdate: (record) => {
+        record.updatedAt = moment().unix();
+      },
+    },
+  }
+);
+export default Expense;
