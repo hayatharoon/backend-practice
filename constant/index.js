7df93baf8e6437eb02062742694c0269bf8bcbb0
Haroon Hayat <hayat_haroon@yahoo.com>
2024-05-05 16:53:09 +0500

    employee modal completye

---
+++ constant/index.js
@@ -0,0 +1,1 @@
+export const GENDER_OPTIONS = Object.freeze(['male', 'female', 'other']);
---
+++ models/employee.js
@@ -0,0 +1,65 @@
+const moment = require('moment');
+const { Sequelize, DataTypes } = require('sequelize');
+const { GENDER_OPTIONS } = require('../constant');
+const sequelize = new Sequelize('sqlite::memory:');
+
+const Employee = sequelize.define(
+  'employee',
+  {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    DOB: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    gender: {
+      type: DataTypes.ENUM,
+      values: GENDER_OPTIONS,
+      allowNull: true,
+    },
+    photo: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: (record) => {
+        (record.createdAt = moment().unix()), (record.updatedAt = moment().unix());
+      },
+      beforeUpdate: (record) => {
+        record.updatedAt = moment().unix();
+      },
+    },
+  }
+);
+export default Employee;
