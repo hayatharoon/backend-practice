f2a51739fab69470bca118e7cb8c9ac39f434e6f
Haroon Hayat <hayat_haroon@yahoo.com>
2024-05-05 17:20:59 +0500

    project member modal complete

---
+++ models/projectMembers.js
@@ -0,0 +1,45 @@
+const moment = require('moment');
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+const ProjectMember = sequelize.define(
+  'projectMember',
+  {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
+    projectId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    employeeId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: (record) => {
+        (record.createdAt = moment().unix()), (record.updatedAt = moment().unix());
+      },
+      beforeUpdate: (record) => {
+        record.updatedAt = moment().unix();
+      },
+    },
+  }
+);
