d52696edc552cbeb6ed0cef988940b2a634624ed
Haroon Hayat <hayat_haroon@yahoo.com>
2024-05-27 00:18:04 +0500

    get employee by id api complete

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -21,4 +21,24 @@ module.exports = {
       errorHandler(error, res);
     }
   },
+  getEmployeeById: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const employee = await Employee.findByPk(id);
+      if (employee) {
+        res.status(200).json({
+          status: 'success',
+          message: 'Employee retrieved successfully',
+          data: employee,
+        });
+      } else {
+        res.status(404).json({
+          status: 'fail',
+          message: 'Employee not found',
+        });
+      }
+    } catch (error) {
+      errorHandler(error, res);
+    }
+  },
 };
