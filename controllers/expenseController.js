920092fc4fe33711fae2da1d38300f63f292546a
Haroon Hayat <hayat_haroon@yahoo.com>
2024-06-05 00:47:02 +0500

    write delete api for expense

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -59,4 +59,45 @@ module.exports = {
       errorHandler(error, res);
     }
   },
+  updateExpense: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const expense = await Expense.findByPk(id);
+      if (!expense) {
+        return res.status(404).json({
+          status: 'fail',
+          message: 'Expense not found',
+        });
+      }
+      await expense.update(req.body, {
+        silent: true,
+      });
+      res.status(200).json({
+        status: 'success',
+        message: 'Expense updated successfully',
+        data: expense,
+      });
+    } catch (error) {
+      errorHandler(error, res);
+    }
+  },
+  deleteExpense: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const expense = await Expense.findByPk(id);
+      if (!expense) {
+        return res.status(404).json({
+          status: 'fail',
+          message: 'Expense not found',
+        });
+      }
+      await expense.destroy();
+      res.status(200).send({
+        status: 'success',
+        message: 'Expense deleted successfully',
+      });
+    } catch (error) {
+      errorHandler(error, res);
+    }
+  },
 };
--- routes/expense.js
+++ routes/expense.js
@@ -6,5 +6,7 @@ const controller = require('../controllers/expenseController');
 router.get('/', controller.getExpense);
 router.post('/', controller.createExpense);
 router.get('/:id', controller.getExpenseById);
+router.put('/:id', controller.updateExpense);
+router.delete('/:id', controller.deleteExpense);
 
 module.exports = router;
